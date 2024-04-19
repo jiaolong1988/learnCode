b3badc5386e735d5d70f395584f6b2d68e19f67f
jiaolong1988 <jiaolong1234567@126.com>
2024-04-19 15:30:32 +0800

    以xml格式显示tocken分析的实现

--- 02nand2tetris/src/main/java/nand2tetris/compiler1/JackScaning.java
+++ 02nand2tetris/src/main/java/nand2tetris/compiler1/JackScaning.java
@@ -2,6 +2,7 @@ package nand2tetris.compiler1;
 
 import nand2tetris.compiler1.pubField.Keyword;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,20 +11,46 @@ import java.util.List;
  * @date: 2024/04/08 17:25
  **/
 public class JackScaning {
-
+   public static List<String> infos = new ArrayList<>();
    private static List<String> keywords = Arrays.asList("class", "constructor", "function",
             "method", "field", "static", "var", "int", "char",
             "boolean", "void", "true", "false", "null", "this",
             "let", "do", "if", "else", "while", "return");
 
 
-   private static List<String> symbols = Arrays.asList("{", "}", "(", ")", "[", "]", ".",",",
-            ";", "+", "-", "*", "/", "&", "|", "<", ">","=", "~");
+   private static List<String> symbols = Arrays.asList(
+           "{", "}", "(", ")", "[", "]", ".",
+           ",", ";", "+", "-", "*", "/", "&",
+           "|", "<", ">","=", "~"
+   );
 
    //将jack语言转换为字元
    public static void getJackChar(String code){
-      for(String c: code.split(" ")){
-         jackAnalysis(c.trim());
+
+     // String code = "let length = Keyboard.readInt(\"HOW MANY NUMBERS? \");";
+      //截取字符串信息
+      String stringCode = "";
+      if(code.contains("\"")){
+         int a = code.indexOf("\"");
+         int b = code.lastIndexOf("\"");
+
+         stringCode = code.substring(a, b+1);
+      }
+
+      String ncode = code;
+      if(stringCode.length()>0){
+         //替换字符串信息
+          ncode = code.replace(stringCode," \" ");
+      }
+
+
+      for(String c: ncode.split(" ")){
+
+         if(c.equals("\"")){
+            jackAnalysis(stringCode);
+         }else{
+            jackAnalysis(c.trim());
+         }
       }
    }
 
@@ -34,42 +61,92 @@ public class JackScaning {
       //其他的都是 标识符
 
       String firstChar = "";
+      String nnCode ="";
       if(code.length() > 1){
          firstChar = code.substring(0, 1);
+         nnCode = code.substring(1, code.length());
       }else{
          firstChar = code;
       }
 
       if(symbols.contains(firstChar)){
-         System.out.println(symbol(firstChar));
+
+         infos.add(symbol(firstChar));
+
+        if(nnCode.length()>0){
+           jackAnalysis(nnCode);
+        }
 
       }else if(firstChar.equals("\"")){
-         System.out.println(StringVal(code));
-
-      }else if(getIntType(code)){
-         System.out.println(inVal(code));
+         infos.add(StringVal(code));
 
       }else{
-         //关键字处理
+
+         String kw ="";
+//关键字处理
          for(String keyword: keywords){
             //以关键字开始
             if(code.startsWith(keyword)){
-               System.out.println(keyword(keyword));
+               kw = keyword;
+               break;
+            }
+         }
 
-               String nCode = code.replace(keyword,"").trim();
+         if(kw.length()>0){
+            infos.add(keyword(kw));
+
+            String nCode = code.replace(kw,"").trim();
+            if(nCode.length()>0){
                jackAnalysis(nCode);
+               code="";
+            }else{
+               return;
             }
          }
 
-         //标识符处理
-         char[] chars = code.toCharArray();
-         for(int i=0; i< chars.length; i++){
+//标识符处理
+         if(code.length()>0){
 
-            if(symbols.contains(chars[i])){
-               code.
-               System.out.println(identifier(keyword));
+            //1.检查是否包含特殊字符
+            int symbolFlag = -1;
+            String  symbol= "";
+            char[] chars = code.toCharArray();
+            for(int i=0; i< chars.length; i++){
+               char temp = chars[i];
+
+               if(symbols.contains(String.valueOf(temp))){
+                  symbol= String.valueOf(chars[i]);
+                  symbolFlag = i;
+                  break;
+               }
+            }
+
+            //包含特殊字符处理
+            if(symbolFlag>-1 && symbol.length()>0){
+               String identifier = code.substring(0,symbolFlag);
+               // 0;
+               if(getIntType(identifier)){
+                  //数字
+                  infos.add(inVal(identifier));
+               }else{
+                 // Keyboard.readInt()
+                  infos.add(identifier(identifier));
+               }
+
+               //符号
+               infos.add(symbol(symbol));
+
+               String nCode = code.substring(symbolFlag+1, code.length());
+               if(nCode.length()>0){
+                  jackAnalysis(nCode);
+               }
+
+            }else{
+               //未包含特殊字符处理
+               infos.add(identifier(code));
             }
          }
+
 
 
       }
@@ -94,7 +171,18 @@ public class JackScaning {
 
    //符号
    public static String symbol(String currentCode) {
-      return "<symbol> " + currentCode + " </symbol>";
+      String schar="";
+      if(currentCode.equals("<")){
+         schar = "&lt;";
+      }else if(currentCode.equals(">")){
+         schar = "&gt;";
+      } else if(currentCode.equals("&")){
+         schar = "&amp;";
+      }else{
+         schar=currentCode;
+      }
+
+      return "<symbol> " + schar + " </symbol>";
    }
 
    //标识符：类名 方法名 变量名
@@ -109,7 +197,8 @@ public class JackScaning {
 
    //字符常量
    public static String StringVal(String currentCode) {
-      return "<stringConstant> " + currentCode + " </stringConstant>";
+      String scode = currentCode.replace("\"", "");
+      return "<stringConstant> " + scode + " </stringConstant>";
    }
 
 }
--- 02nand2tetris/src/main/java/nand2tetris/compiler1/ServerMainCompiler1.java
+++ 02nand2tetris/src/main/java/nand2tetris/compiler1/ServerMainCompiler1.java
@@ -3,6 +3,10 @@ package nand2tetris.compiler1;
 import nand2tetris.CommonUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,42 +17,56 @@ import java.util.List;
  */
 public class ServerMainCompiler1 {
     public static void main(String[] args) {
-        String file = "D:\\test\\10\\ArrayTest\\Main.jack";
+
+        String dirPath  = "D:\\test\\10\\Square";
+        File dir = new File(dirPath);
+        for(File file : dir.listFiles()){
+            if(file.getName().contains(".jack")){
+                alalysisFileToXml(file.getAbsolutePath());
+            }
+        }
+
+        //String file = "D:\\test\\10\\ArrayTest\\Main.jack";
+        //String file  = "D:\\test\\10\\Square\\Square.jack";
+        // String file  = "D:\\test\\10\\Square\\SquareGame.jack";
+        // alalysisFileToXml(file);
+    }
+
+    private static void alalysisFileToXml(String file) {
+        JackScaning.infos.clear();
         File f = new File(file);
 
+        JackScaning.infos.add("<tokens>");
         List<String> jackCodes = readJackFile(f);
         for(String code: jackCodes){
+            //System.out.println("--> "+code);
             JackScaning.getJackChar(code);
         }
 
-
-
-//        JackTokenizer jackTokenizer = new JackTokenizer(f);
-//        while (true){
-//            boolean flag = jackTokenizer.advance();
-//            if(!flag){
-//                break;
-//            }
-//        }
-
+        JackScaning.infos.add("</tokens>");
+        outputFile(f.toPath(),JackScaning.infos,"_T");
     }
 
 
-   private static List<String> readJackFile(File jackFile) {
+    private static List<String> readJackFile(File jackFile) {
        //读取文件内容
        List<String> jackInfo = CommonUtils.readFile(jackFile.toPath());
 
        //内容过滤
        List<String> list = new ArrayList<>();
-       for (String info : jackInfo) {
+       for (String jack : jackInfo) {
 
+           String info = jack.trim();
            String command = "";
            if (info.contains("/**")) {
                command = info.substring(0, info.indexOf("/**")).trim();
 
            } else if (info.contains("//")) {
                command = info.substring(0, info.indexOf("//")).trim();
-           } else {
+
+           } else if (info.startsWith("*") || info.startsWith("*/")) {
+               command="";
+           }else {
                command = info.trim();
            }
 
@@ -59,5 +77,16 @@ public class ServerMainCompiler1 {
         return list;
    }
 
+   public static void outputFile(Path p, List<String> binaryProgram, String type) {
+        String fileName = p.toAbsolutePath().toString();
+        String outFileName = fileName.substring(0, fileName.lastIndexOf(".") )+type+".xml";
+
+        Path outPath = Paths.get(outFileName);
+        try {
+            Files.write(outPath, binaryProgram);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
