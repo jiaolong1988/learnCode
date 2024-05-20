4f2fedbe03bbb563681f920d5c8f38698c7c2752
jiaolong1988 <jiaolong1234567@126.com>
2024-05-20 16:17:36 +0800

    编译器I代码最终版本

--- 02nand2tetris/src/main/java/nand2tetris/compiler1/JackTokenizer.java
+++ 02nand2tetris/src/main/java/nand2tetris/compiler1/JackTokenizer.java
@@ -1,11 +1,11 @@
 package nand2tetris.compiler1;
 
-import nand2tetris.CommonUtils;
 import nand2tetris.compiler1.pubField.Keyword;
 import nand2tetris.compiler1.pubField.TokenType;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -17,86 +17,179 @@ import java.util.List;
  */
 public class JackTokenizer {
 
-    private Iterator<String> iterator = null;
+    private static List<String> keywords = Arrays.asList("class", "constructor", "function",
+            "method", "field", "static", "var", "int", "char",
+            "boolean", "void", "true", "false", "null", "this",
+            "let", "do", "if", "else", "while", "return");
+
+
+    private static List<String> symbols = Arrays.asList(
+            "{", "}", "(", ")", "[", "]", ".",
+            ",", ";", "+", "-", "*", "/", "&",
+            "|", "<", ">","=", "~"
+    );
+
+
     //当前Jack内容
     private String currentCommand;
+    private boolean stringTypeFlag = false;
+
+    public  List<String> jackCodes;
+    public  List<String> jackLineCode = new ArrayList<>();
+    private Iterator<String> lineIterator;
+
+    private List<String> tockens = new ArrayList();
 
     public JackTokenizer(File inputFilePath) {
-        //读取文件内容
-        List<String> jackInfo = CommonUtils.readFile(inputFilePath.toPath());
+        jackCodes = ReadJackFileUtil.readJackFile(inputFilePath);
+//        for (String jackCode : jackCodes) {
+//            splitCode(jackCode);
+//        }
 
-        //内容过滤
-        List<String> list = new ArrayList<>();
-        for(String info :jackInfo){
-
-            String command = "";
-            if(info.contains("/**")){
-                command = info.substring(0,info.indexOf("/**")).trim();
-
-            }else if(info.contains("//")){
-                command = info.substring(0,info.indexOf("//")).trim();
-            }else{
-                command = info.trim();
-            }
-
-            if(command.length()>0){
-                list.add(command);
-            }
-        }
-        iterator = list.iterator();
+      //  iterator = jackCodes.iterator();
     }
 
+    public void splitCode(String jackCode) {
+        jackLineCode.clear();
+        StringBuilder info = new StringBuilder();
+        for (int i = 0; i < jackCode.length(); i++) {
+            char c = jackCode.charAt(i);
+            info.append(c);
+
+            //空格
+            if (Character.isWhitespace(c) && stringTypeFlag==false) {
+                printInfo(info.toString());
+                info.setLength(0);
+
+            }
+            //符号
+            else if(symbols.contains(String.valueOf(c))){
+                printSymbols(info.toString());
+                info.setLength(0);
+
+            }
+            //字符串
+            else if("\"".equals(String.valueOf(c)) || stringTypeFlag==true){
+                if (stringTypeFlag==true &&  String.valueOf(c).equals("\"") ){
+                    printInfo(info.toString());
+                    info.setLength(0);
+                    stringTypeFlag=false;
+                }else{
+                    stringTypeFlag=true;
+                }
+            }
+        }
+
+        lineIterator = jackLineCode.iterator();
+    }
+
+    private void printInfo(String info) {
+        String nstring = info.trim();
+        if(nstring.length()>0){
+           // System.out.println(nstring);
+            jackLineCode.add(nstring);
+        }
+    }
+
+    private void printSymbols(String info) {
+        String nstring = info.substring(0,info.length()-1).trim();
+        printInfo(nstring);
+
+        String symbol = info.substring(info.length()-1).trim();
+       // System.out.println(symbol);
+        jackLineCode.add(symbol);
+    }
+
+
     public boolean hasMoreTokens() {
-        return iterator.hasNext();
+        return lineIterator.hasNext();
     }
 
     //读取下一条指令，并将其设置为当前命令
     public boolean advance(){
         boolean flag = hasMoreTokens();
         if(flag){
-            currentCommand = iterator.next().trim();
-            System.out.println("jack info："+currentCommand);
+            currentCommand = lineIterator.next().trim();
         }
 
         return flag;
     }
 
     public TokenType tokenType() {
+        if(symbols.contains(currentCommand)){
+            tockens.add( symbol());
+            return  TokenType.SYMBOL;
+        }
 
-        return TokenType.IDENTIFIER;
+        if (currentCommand.contains("\"")){
+            tockens.add( StringVal());
+            return TokenType.STRING_CONST;
+        }
+
+        if (keywords.contains(currentCommand)){
+            tockens.add(keyword());
+            return  TokenType.KEYWORD;
+        }
+
+        //由数字字母下划线组成，其中，不能以数字开头
+       if( currentCommand.matches("^[a-zA-Z_]{1}[a-zA-Z0-9_]*")){
+           tockens.add(identifier());
+           return TokenType.IDENTIFIER;
+       }
+
+        if( currentCommand.matches("\\d+")){
+            tockens.add(inVal());
+            return TokenType.INT_CONST;
+        }
+
+        System.out.println(currentCommand);
+        return null;
     }
 
     //字元的5中类型1
-    public Keyword keyword() {
+ //   public Keyword keyword() {
+    public String keyword() {
     //<keyword>  </keyword>
-
-        return Keyword.BOOLEAN;
+        return "<keyword> " + currentCommand + " </keyword>";
+      //  return Keyword.BOOLEAN;
     }
 
     //符号
     public String symbol() {
     //<symbol>  </symbol>
+        String schar="";
+        if(currentCommand.equals("<")){
+            schar = "&lt;";
+        }else if(currentCommand.equals(">")){
+            schar = "&gt;";
+        } else if(currentCommand.equals("&")){
+            schar = "&amp;";
+        }else{
+            schar=currentCommand;
+        }
 
-        return null;
+        return "<symbol> " + schar + " </symbol>";
     }
 
     //标识符：类名 方法名 变量名
     public String identifier() {
     //<identifier>  </identifier>
-
-        return null;
+        return "<identifier> " + currentCommand + " </identifier>";
     }
 
     //整数常量
-    public int inVal() {
+    public String inVal() {
     //<integerConstant>  </integerConstant>
-        return 0;
+        return "<integerConstant> " + currentCommand + " </integerConstant>";
     }
 
     //字符常量
     public String StringVal() {
         //<stringConstant> </stringConstant>
-        return null;
+        return "<stringConstant> " + currentCommand.replace("\"","") + " </stringConstant>";
     }
 
+    public List<String> getTockens() {
+        return tockens;
+    }
 }
--- 02nand2tetris/src/main/java/nand2tetris/compiler1/ServerMainCompiler1.java
+++ 02nand2tetris/src/main/java/nand2tetris/compiler1/ServerMainCompiler1.java
@@ -10,7 +10,30 @@ import java.util.List;
  */
 public class ServerMainCompiler1 {
     public static void main(String[] args) {
+       // testTockens();
+        testCompilation();
+    }
 
+    public static void testTockens(){
+        String dirPath  = "D:\\test\\10\\ExpressionLessSquare\\Square.jack";
+        File dir = new File(dirPath);
+
+        JackTokenizer jackTokenizer = new JackTokenizer(dir);
+        jackTokenizer.getTockens().add("<tokens>");
+        for(String jackCode : jackTokenizer.jackCodes){
+            jackTokenizer.splitCode(jackCode);
+
+            while(jackTokenizer.advance()){
+                jackTokenizer.tokenType();
+            }
+        }
+        jackTokenizer.getTockens().add("</tokens>");
+        jackTokenizer.getTockens().forEach(System.out::println);
+
+        ReadJackFileUtil.outputFile(dir.toPath(), jackTokenizer.getTockens(),"_TT");
+
+    }
+    public static void testCompilation(){
         String file  = "D:\\test\\10\\ExpressionLessSquare\\SquareGameT.xml";
         File inputFile = new File(file);
 
@@ -18,35 +41,35 @@ public class ServerMainCompiler1 {
         ce.compileClass();
     }
 
-    public static void tets1(){
-        String dirPath  = "D:\\test\\10\\Square";
-        File dir = new File(dirPath);
-        for(File file : dir.listFiles()){
-            if(file.getName().contains(".jack")){
-                alalysisFileToXml(file.getAbsolutePath());
-            }
-        }
-
-        //String file = "D:\\test\\10\\ArrayTest\\Main.jack";
-        //String file  = "D:\\test\\10\\Square\\Square.jack";
-        // String file  = "D:\\test\\10\\Square\\SquareGame.jack";
-        // alalysisFileToXml(file);
-    }
-
-    private static void alalysisFileToXml(String file) {
-        JackScaning.infos.clear();
-        File f = new File(file);
-
-        JackScaning.infos.add("<tokens>");
-        List<String> jackCodes = ReadJackFileUtil.readJackFile(f);
-        for(String code: jackCodes){
-            //System.out.println("--> "+code);
-            JackScaning.getJackChar(code);
-        }
-
-        JackScaning.infos.add("</tokens>");
-        ReadJackFileUtil.outputFile(f.toPath(),JackScaning.infos,"_T");
-    }
+//    public static void tets1(){
+//        String dirPath  = "D:\\test\\10\\Square";
+//        File dir = new File(dirPath);
+//        for(File file : dir.listFiles()){
+//            if(file.getName().contains(".jack")){
+//                alalysisFileToXml(file.getAbsolutePath());
+//            }
+//        }
+//
+//        //String file = "D:\\test\\10\\ArrayTest\\Main.jack";
+//        //String file  = "D:\\test\\10\\Square\\Square.jack";
+//        // String file  = "D:\\test\\10\\Square\\SquareGame.jack";
+//        // alalysisFileToXml(file);
+//    }
+//
+//    private static void alalysisFileToXml(String file) {
+//        JackScaning.infos.clear();
+//        File f = new File(file);
+//
+//        JackScaning.infos.add("<tokens>");
+//        List<String> jackCodes = ReadJackFileUtil.readJackFile(f);
+//        for(String code: jackCodes){
+//            //System.out.println("--> "+code);
+//            JackScaning.getJackChar(code);
+//        }
+//
+//        JackScaning.infos.add("</tokens>");
+//        ReadJackFileUtil.outputFile(f.toPath(),JackScaning.infos,"_T");
+//    }
 
 
 }
