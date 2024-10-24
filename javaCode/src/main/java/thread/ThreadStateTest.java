8b941d9d61febff9657fdbbb695f5b098cd40d44
jiaolong1988 <jiaolong1234567@126.com>
2024-10-24 10:22:02 +0800

    增加注释

--- javaCode/src/main/java/thread/ThreadStateTest.java
+++ javaCode/src/main/java/thread/ThreadStateTest.java
@@ -1,13 +1,12 @@
 package thread;
 
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
 
 /**
  * 1. 初始(NEW)：新创建了一个线程对象，但还没有调用start()方法。
  * 2. 运行(RUNNABLE)：Java线程中将就绪（ready）和运行中（running）两种状态笼统的称为“运行”。
  * 线程对象创建后，其他线程(比如main线程）调用了该对象的start()方法。该状态的线程位于可运行线程池中，等待被线程调度选中，获取CPU的使用权，此时处于就绪状态（ready）。就绪状态的线程在获得CPU时间片后变为运行中状态（running）。
- * 3. 阻塞(BLOCKED)：表示线程阻塞于锁。
+ * 3. 阻塞(BLOCKED)：表示线程阻塞于 一个对象锁。
  * 4. 等待(WAITING)：进入该状态的线程需要等待其他线程做出一些特定动作（通知或中断）。
  * 5. 超时等待(TIMED_WAITING)：该状态不同于WAITING，它可以在指定的时间后自行返回。
  * 6. 终止(TERMINATED)：表示该线程已经执行完毕。
@@ -18,11 +17,11 @@ import com.sun.org.apache.bcel.internal.generic.NEW;
  **/
 public class ThreadStateTest {
     public static void main(String[] args) throws InterruptedException {
-        //testNew();        // 0
+       // testNew();        // 0
         //testRunnable();   //2
-        //testBlocked();    //5
-        //testWaiting();      //401
-        //testTimedWaiting();
+      //  testBlocked();    //5
+        testWaiting();      //401
+       // testTimedWaiting();
         //testTerminated();
     }
 
@@ -55,8 +54,7 @@ public class ThreadStateTest {
 
     /**
      * WAITING-无时间参数等待
-     *
-     * @return: void
+     * main线程 必须 等待 thread2线程完成，此时main线程为WAITING状态
      **/
     public static void testWaiting() throws InterruptedException {
         //3.WAITING-无时间参数等待
@@ -77,6 +75,9 @@ public class ThreadStateTest {
     }
 
 
+    /**
+     *  线程没有执行完毕，在线程外部 查看 线程状态为TIMED_WAITING状态
+     **/
     public static void testTimedWaiting() throws InterruptedException {
         //4.TIMED_WAITING
         Thread thread3 = new Thread(() -> {
