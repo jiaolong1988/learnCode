5094ebb8a411d7c3311b33f57b4aaf4594ac0c97
jiaolong1988 <jiaolong1234567@126.com>
2025-01-16 17:19:38 +0800

    调整案例

--- javaCode/src/main/java/thread/define/ThirdThread.java
+++ javaCode/src/main/java/thread/define/ThirdThread.java
@@ -4,32 +4,36 @@ import java.util.concurrent.*;
 
 public class ThirdThread {
 	public static void main(String[] args){
-		// 创建Callable对象
-		ThirdThread rt = new ThirdThread();
 		// 先使用Lambda表达式创建Callable<Integer>对象
 		// 使用FutureTask来包装Callable对象
 		FutureTask<Integer> task = new FutureTask<Integer>((Callable<Integer>)() -> {
 			int i = 0;
 			for ( ; i < 100 ; i++ ) {
-				System.out.println(Thread.currentThread().getName()
-						+ " 的循环变量i的值：" + i);
+				System.out.println(Thread.currentThread().getName() + " 的循环变量i的值：" + i);
+				TimeUnit.MILLISECONDS.sleep(100);
 			}
 			// call()方法可以有返回值
 			return i;
 		});
 
-		for (int i = 0 ; i < 100 ; i++) {
-			System.out.println(Thread.currentThread().getName()
-					+ " 的循环变量i的值：" + i);
-			if (i == 20){
-				// 实质还是以Callable对象来创建、并启动线程
-				new Thread(task , "有返回值的线程").start();
-			}
+		//t1的状态：TIMED_WAITING
+		Thread t1 = new Thread(task);
+		t1.start();
+		System.out.println("启动一个线程：t1");
+
+		//t2的状态：TERMINATED,启动的时候就终止了。
+		Thread t2 = new Thread(task);
+		t2.start();
+		System.out.println("启动一个线程：t2");
+
+		for(int i = 0 ; i < 100 ; i++) {
+			System.out.println(i+"_t1的状态："+t1.getState());
+			System.out.println(i+"_t2的状态："+t2.getState());
 		}
 
 		try {
 			// 获取线程返回值
-			System.out.println("子线程的返回值：" + task.get());
+			System.out.println("==>线程返回值：" + task.get());
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
